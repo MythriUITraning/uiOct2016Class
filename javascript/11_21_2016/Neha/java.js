15d8169a21e341a7ebdba32a6ac3c38c2f5c26d2
Your Name <you@example.com>
2016-11-22 16:00:18 -0800

    JsAssingment-1

---
+++ javascript/11_21_2016/Neha/java.js
@@ -0,0 +1,27 @@
+// for number
+var a = 2;
+
+// for float value
+var b = 7.3;
+
+// for string values
+var c = 'Neha';
+
+// for boolean
+var d = true;
+var e = false;
+
+// for null value
+var f = null;
+
+//for undefined value
+var g = undefined;
+
+// console
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
