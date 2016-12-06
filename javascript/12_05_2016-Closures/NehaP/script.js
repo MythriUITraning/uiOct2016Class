448ed28ede9af3971dc27cfc179061c5d7eba552
Your Name <you@example.com>
2016-12-06 06:44:23 -0800

    JS Class and Closure Assingment

---
+++ javascript/12_05_2016-Closures/NehaP/script.js
@@ -0,0 +1,33 @@
+(function(){
+    //defining calss called total
+    function total(){
+        var _cal = this;
+        _cal.addition = 0;
+        _cal.substract = 0;
+        _cal.product = 0;
+        _cal.dividend = 0;
+        // defining another class called mathematics inside the total class
+        function mathematics(x , y){
+            var _mat = this;
+            _mat.sum = function(){
+                return (x + y)
+            };
+            _mat.difference = function(){
+                return(x - y)
+            };
+            _mat.multiplication = function(){
+                return(x * y)
+            };
+            _mat.division = function(){
+                return(x / y)
+            };
+        }
+            var num = new mathematics(11 , 2);
+            _cal.addition = num.sum();
+            _cal.substract = num.difference();
+            _cal.product = num.multiplication();
+            _cal.dividend = num.division();
+    }
+        var ans = new total();
+        console.log(ans);
+})();
