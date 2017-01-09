087e1a52c62b80075dc06f97edc7d5f69d550c72
Your Name <you@example.com>
2017-01-08 22:03:22 -0800

    Angular project1

---
+++ angular/neha/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nehaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nehaApp
+ */
+angular.module('nehaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
