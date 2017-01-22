14ea4466f541d25e0ee8076eb3c6b4857611c24d
Your Name <you@example.com>
2017-01-21 17:47:31 -0800

    My first Angular App 01_21_2017

---
+++ angular/Neha/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nehaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nehaApp
+ */
+angular.module('nehaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
